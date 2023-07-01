98c0468d63f41120822a67e44cea4ee056338cec
Pranita <pfulsundar8@gmail.com>
2023-07-01 22:20:45 +0530

    styles: fix CSS

--- src/pages/RecipeDetail.jsx
+++ src/pages/RecipeDetail.jsx
@@ -9,23 +9,27 @@ export const RecipeDetail = () => {
     (recipe) => recipe.id === parseInt(recipeId)
   );
   return (
-    <div className="p-2 text-center">
-      <h1 className="text-3xl py-2">{recipe?.name}</h1>
-      <div className="flex shadow-2xl rounded-md">
-        <div>
-          <img src={recipe?.imgUrl} alt={recipe?.name} className="" />
+    <div className="p-2 text-center  ">
+      <h1 className="text-3xl py-2 my-2">{recipe?.name}</h1>
+      <div className="flex shadow-2xl rounded-md w-[80%] m-auto my-8">
+        <div className="w-[100rem]">
+          <img
+            src={recipe?.imgUrl}
+            alt={recipe?.name}
+            className="w-full h-full rounded object-cover"
+          />
         </div>
-        <div className="p-2">
-          <div>
-            <span>Cuisine:</span>
+        <div className="p-2 text-start flex flex-col gap-2 my-4 px-4">
+          <div className="text-xl">
+            <span className="font-semibold">Cuisine: </span>
             <span>{recipe?.cuisine}</span>
           </div>
           <div>
-            <span>Ingredients:</span>
-            <span>{recipe?.ingredients}</span>
+            <span className="font-semibold">Ingredients: </span>
+            <span>{recipe?.ingredients.join(", ")}</span>
           </div>
           <div>
-            <span>Instructions:</span>
+            <span className="font-semibold">Instructions: </span>
             <span>{recipe?.instructions}</span>
           </div>
         </div>
